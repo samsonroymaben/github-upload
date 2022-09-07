9e785370ae38e51ed3437d357b724bb8982aacda
samsonroy <samsonmaben@gmail.com>
2022-09-07 14:34:42 +0530

    draft

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Samson");
+// console.log("222");
+// const user = {
+//   name: "Daniel",
+//   age: 26,
+// };
+// user.location
---
+++ index.ts
@@ -0,0 +1,15 @@
+console.log("Samson");
+
+// console.log("222");
+type USER = {
+  name: string;
+  age: number;
+//   location: string;
+};
+const user: USER = {
+  name: "Daniel",
+  age: 26,
+//   location: "Hhee",
+};
+
+user.age
